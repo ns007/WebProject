614df4d01dd6a748a4957c63f40720f4872e4faf
ns007 <ns0070@walla.co.il>
2016-01-14 16:24:34 +0200

    spotinfo marker function

--- includes/js/mySpotGoogleMapScripts.js
+++ includes/js/mySpotGoogleMapScripts.js
@@ -18,6 +18,9 @@ function initMap() {
         title: 'Sironit Beach'
     });
 
+	 marker.addListener('click', function() {
+	    window.open ('spotinfo.html','_self',false)
+  	});
     var marker2 = new google.maps.Marker({
         position: {lat: 32.3083869, lng: 34.8437168},
         map: map,
